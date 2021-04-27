67c716d3d7139bc9523168b55184571a3aa4fc04
Kevin Giron <kg19882020@gmail.com>
2021-04-26 23:48:15 -0700

    created a variable of uppercase letters

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-//array of numbers for the password later
+//arrays of different kinds for the password later
 var numbers = [ "0", "1", "2", "3", "4", "5", "6","7","8","9"] ;
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+                        "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
